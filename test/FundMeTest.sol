0e145cf9f072a5300a2a3f4a24a23c95b4413de1
tzafff <christzaf99@gmail.com>
2024-04-20 19:08:49 +0300

    Refactor Code for better Modularity

--- test/FundMeTest.sol
+++ test/FundMeTest.sol
@@ -3,13 +3,16 @@ pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test{
 
     FundMe fundMe;
     
     function setUp() external {
-        fundMe = new FundMe();
+        //fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
     }
 
     function testMinimumDollarIsFive() public view{
@@ -19,7 +22,14 @@ contract FundMeTest is Test{
     function testOwnerIsMsgSender() public view {
         // us --> FundMeTEST --> FundMe
         // So the deployer of the FundMe is that TestContract "FundMeTest"
-        assertEq(fundMe.i_owner(), address(this));
+        //assertEq(fundMe.i_owner(), address(this));
 
+        // Now the DeployFundMe.s.sol is the deployer because of vm.startBroadcast makes the funder msg.sender again.
+        assertEq(fundMe.i_owner(), msg.sender);
+    }
+
+    function testPriceFeedVersionIsAccurate() public view{
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4);
     }
 }
